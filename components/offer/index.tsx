ba9a5826cbaa6eae117dcd18048dff70bb7c3632
jaime <garciadiazjaime@gmail.com>
2023-07-23 15:24:40 -0500

    feat: day component

--- components/offer/index.tsx
+++ components/offer/index.tsx
@@ -1,13 +1,7 @@
 import { useState } from "react";
-const days = [
-  "sunday",
-  "monday",
-  "tuesday",
-  "wednesday",
-  "thursday",
-  "friday",
-  "saturday",
-];
+
+import Day from "./day";
+import { days } from './support'
 
 export default function Offer() {
   const [selectedDays, setSelectedDays] = useState<string[]>([]);
@@ -48,54 +42,3 @@ export default function Offer() {
     </div>
   );
 }
-
-const Day = ({
-  index,
-  selectedDays,
-  setSelectedDays,
-}: {
-  index: number;
-  selectedDays: string[];
-  setSelectedDays: (day: string) => void;
-}) => {
-  const formatter = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  });
-  const price = 59;
-  const blockDates = [6];
-
-  const day = new Date();
-  day.setDate(day.getDate() + index);
-
-  const isBlock = blockDates.includes(day.getDay());
-
-  const dayClickHandler = () => {
-    if (!isBlock) {
-        setSelectedDays(`${day.getMonth()}-${day.getDate()}`);
-    }
-  };
-
-  const isDaySelected = selectedDays.includes(
-    `${day.getMonth()}-${day.getDate()}`
-  );
-
-  return (
-    <div
-      style={{
-        padding: 20,
-        border: "1px solid black",
-        width: 120,
-        cursor: isBlock ? "" : "pointer",
-        backgroundColor: isBlock ? "#DDD" : isDaySelected ? "#AAF" : "",
-      }}
-      onClick={dayClickHandler}
-    >
-      <div style={{ textTransform: "capitalize" }}>{days[day.getDay()]}</div>
-      <div style={{ margin: "6px 0" }}>
-        {day.getDate()} / {day.getMonth()}
-      </div>
-      <div>{formatter.format(price)}</div>
-    </div>
-  );
-};
