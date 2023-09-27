0afffd7da8b5c9d793c94446883f07b8eeabcc8d
jaime <garciadiazjaime@gmail.com>
2023-09-26 23:21:04 -0500

    feat: update cabins img

--- components/cabins.tsx
+++ components/cabins.tsx
@@ -6,21 +6,21 @@ import useViewport from "@/utils/useViewport";
 const items = [
   {
     link: "https://www.airbnb.com/rooms/897016284894923633",
-    image: "/cabins/cabin-1/front.webp",
+    image: "/cabins/cabin-1/original/front.jpg",
     backgroundColor: "#DE3151",
     name: "Cabin #1",
     target: "_blank",
   },
   {
     link: "https://www.airbnb.com/rooms/901621862087069881",
-    image: "/cabins/cabin-2/bedroom.webp",
+    image: "/cabins/cabin-2/original/front.jpg",
     backgroundColor: "#d93b30",
     name: "Cabin #2",
     target: "_blank",
   },
   {
     link: "https://www.airbnb.com/rooms/901624640241144955",
-    image: "/cabins/cabin-3/kitchen.webp",
+    image: "/cabins/cabin-3/original/front.jpg",
     backgroundColor: "#bd196d",
     name: "Cabin #3",
     target: "_blank",
